3cc20980d553fbb50173cd3c09fa55189e5b762b
kaitoyamagishi1109 <kaitoy@bu.edu>
2020-01-12 21:55:37 +0900

    successful button display w/ hidden credentials

--- OutChair.js
+++ OutChair.js
@@ -1,1 +1,16 @@
-console.log("Test Change!");
+console.log("Test Change!\n");
+
+function renderButton() {
+    gapi.signin2.render('GoogleButton',
+    {
+       'onsuccess': () => {},
+       'onfailure': () => {}
+   });
+}
+
+function init() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init({client_id : config.OAUTH_CLIENT_ID});
+        renderButton();
+    });
+}
