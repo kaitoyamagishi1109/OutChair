d048284a7d5af434de4ae8aa119943d888e56785
Kaito Yamagishi <kaitoyamagishi1109@gmail.com>
2020-09-05 15:07:25 -0400

    Update OutChair.js

--- OutChair.js
+++ OutChair.js
@@ -1,16 +1,7 @@
-console.log("Test Change!\n");
-
-function renderButton() {
-    gapi.signin2.render('GoogleButton',
-    {
-       'onsuccess': () => {},
-       'onfailure': () => {}
-   });
-}
-
-function init() {
-    gapi.load('auth2', function() {
-        gapi.auth2.init({client_id : config.OAUTH_CLIENT_ID});
-        renderButton();
-    });
-}
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
