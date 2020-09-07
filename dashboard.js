07fb29acc52161b79d4e3356bc8c43c5c8365109
Kaito Yamagishi <kaitoyamagishi1109@gmail.com>
2020-09-07 18:36:44 -0400

    added random temp&humidity

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,17 @@
-var temperature = new Array(13);
+var temperature = [64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76];
+var humidity = [28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64];
 
-for (i=0; i<13; i++) {
-  temperature[i] = (64+i.toFixed(0)).toString;
+var randTemp = Array(6);
+var randHumidity = Array(6);
+
+for (i=0; i<6; i++) {
+  var randInt = getRndInteger(0,13);
+  randTemp[i] = "Temperature: " + temperature[randInt].toString() + "°F";
+  randHumidity[i] = "Humidity: " + humidity[randInt].toString() + "%";
+  document.getElementById("temp"+i.toString()).innerHTML = randTemp[i];
+  document.getElementById("humidity"+i.toString()).innerHTML = randHumidity[i];
 }
 
-console.log(temperature);
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
